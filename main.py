099a66bb9945aad3c2003236069f3c72afe89136
JillLeah <jillianlweiss@gmail.com>
2025-07-02 14:46:55 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-def read_index():
-    return FileResponse("static/index.html")
